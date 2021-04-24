ac789cfac3ecb984e50427cc01b659dc268757d6
peknight <JKpeknight@gmail.com>
2021-04-24 18:45:27 +0800

    update fpinscala chapter 7

--- demo-fp/src/main/scala/com/peknight/demo/fpinscala/parallelism/Par.scala
+++ demo-fp/src/main/scala/com/peknight/demo/fpinscala/parallelism/Par.scala
@@ -1,20 +1,35 @@
 package com.peknight.demo.fpinscala.parallelism
 
-trait Par[+A] {
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
-}
+//trait Par[+A] {
+//
+//}
 object Par {
-  def unit[A](a: A): Par[A] = ???
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, unit: TimeUnit): A = get
+    def isCancelled: Boolean = false
+    def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+  }
+
+  // Exercise 7.1 7.3
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
+    val af = a(es)
+    val bf = b(es)
+    UnitFuture(f(af.get, bf.get))
+  }
+
+  def fork[A](a: => Par[A]): Par[A] = es => es.submit(() => a(es).get)
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  @deprecated("use run")
-  def get[A](a: Par[A]): A = run(a)
+//  @deprecated("use run")
+//  def get[A](a: Par[A]): A = run(a)
 
-  def run[A](a: Par[A]): A = ???
-
-  // Exercise 7.1
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
-
-  def fork[A](a: => Par[A]): Par[A] = ???
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 }
--- demo-fp/src/main/scala/com/peknight/demo/fpinscala/parallelism/ParallelismApp.scala
+++ demo-fp/src/main/scala/com/peknight/demo/fpinscala/parallelism/ParallelismApp.scala
@@ -1,5 +1,7 @@
 package com.peknight.demo.fpinscala.parallelism
 
+import com.peknight.demo.fpinscala.parallelism.Par._
+
 object ParallelismApp extends App {
   def sum(ints: Seq[Int]): Int =
     ints.foldLeft(0)((a, b) => a + b)
@@ -11,13 +13,13 @@ object ParallelismApp extends App {
     }
   }
 
-  def sumViaParV1(ints: IndexedSeq[Int]): Int =
-    if (ints.size <= 1) ints.headOption getOrElse 0 else {
-      val (l, r) = ints.splitAt(ints.length / 2)
-      val sumL: Par[Int] = Par.unit(sumViaParV1(l))
-      val sumR: Par[Int] = Par.unit(sumViaParV1(r))
-      Par.run(sumL) + Par.run(sumR)
-    }
+//  def sumViaParV1(ints: IndexedSeq[Int]): Int =
+//    if (ints.size <= 1) ints.headOption getOrElse 0 else {
+//      val (l, r) = ints.splitAt(ints.length / 2)
+//      val sumL: Par[Int] = Par.unit(sumViaParV1(l))
+//      val sumR: Par[Int] = Par.unit(sumViaParV1(r))
+//      Par.run(sumL) + Par.run(sumR)
+//    }
 
   def sumViaParV2(ints: IndexedSeq[Int]): Par[Int] =
     if (ints.size <= 1)
