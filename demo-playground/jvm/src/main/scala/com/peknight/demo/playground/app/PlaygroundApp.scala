635654dd0ba23bf544f2726e1371f50dc6df95a1
peknight <JKpeknight@gmail.com>
2022-05-02 11:25:11 +0800

    format

--- demo-playground/jvm/src/main/scala/com/peknight/demo/playground/app/PlaygroundApp.scala
+++ demo-playground/jvm/src/main/scala/com/peknight/demo/playground/app/PlaygroundApp.scala
@@ -2,5 +2,5 @@ package com.peknight.demo.playground.app
 
 import cats.effect.{IO, IOApp}
 
-object PlaygroundApp extends IOApp.Simple :
+object PlaygroundApp extends IOApp.Simple:
   def run = IO.println("Hello, world!")
