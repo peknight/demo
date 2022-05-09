ba41e0556f4998250c65c37f9f56a1d0fdc21b97
peknight <JKpeknight@gmail.com>
2022-05-09 13:26:30 +0800

    update middleware

--- demo-http4s/jvm/src/main/scala/com/peknight/demo/http4s/middleware/MiddlewareApp.scala
+++ demo-http4s/jvm/src/main/scala/com/peknight/demo/http4s/middleware/MiddlewareApp.scala
@@ -6,12 +6,13 @@ import cats.syntax.all.*
 import org.http4s.*
 import org.http4s.dsl.io.*
 import org.http4s.implicits.*
+import org.http4s.server.Middleware
 
 /**
  * 中间件是对服务(service)的一个包装，它提供了对传入服务的请求(Request)和服务返回的响应(Response)做操作的能力，甚至可以阻止服务被调用
  * 中间件通常是一个简单的接收一个服务作为入参并返回另一个服务的函数
  */
-object MiddlewareApp:
+object MiddlewareApp extends IOApp.Simple:
 
   given CanEqual[Path, Path] = CanEqual.derived
   given CanEqual[Method, Method] = CanEqual.derived
@@ -27,4 +28,48 @@ object MiddlewareApp:
     case GET -> Root / "bad" => BadRequest()
     case _ => Ok()
   }
+
+  val goodRequest = Request[IO](Method.GET, uri"/")
+  val badRequest = Request[IO](Method.GET, uri"/bad")
+
+  val wrappedService = myMiddle(service, "SomeKey" -> "SomeValue")
+
+  // 如果想要中间件在多处复用，也可以将中间件定义为object使用apply方法
+  object MyMiddle:
+    def addHeader(resp: Response[IO], header: Header.ToRaw) = resp match
+      case Status.Successful(resp) => resp.putHeaders(header)
+      case resp => resp
+    def apply(service: HttpRoutes[IO], header: Header.ToRaw) = service.map(addHeader(_, header))
+
+  val newService = MyMiddle(service, "SomeKey" -> "SomeValue")
+
+  // 中间件可以支持入参服务的请求、响应 出参服务的请求、响应 分别使用不同的类型
+  type AuthMiddleware[F[_], T] = Middleware[F, AuthedRequest[F, T], Response[F], Request[F], Response[F]]
+
+  val apiService = HttpRoutes.of[IO] {
+    case GET -> Root / "api" => Ok()
+  }
+
+  val aggregateService = apiService <+> MyMiddle(service, "SomeKey" -> "SomeValue")
+  val apiRequest = Request[IO](Method.GET, uri"/api")
+
+  val run =
+    for
+      goodResp <- service.orNotFound(goodRequest)
+      _ <- IO.println(goodResp)
+      badResp <- service.orNotFound(badRequest)
+      _ <- IO.println(badResp)
+      wrappedGoodResp <- wrappedService.orNotFound(goodRequest)
+      _ <- IO.println(wrappedGoodResp)
+      wrappedBadResp <- wrappedService.orNotFound(badRequest)
+      _ <- IO.println(wrappedBadResp)
+      newGoodResp <- newService.orNotFound(goodRequest)
+      _ <- IO.println(newGoodResp)
+      newBadResp <- newService.orNotFound(badRequest)
+      _ <- IO.println(newBadResp)
+      aggregateGoodResp <- aggregateService.orNotFound(goodRequest)
+      _ <- IO.println(aggregateGoodResp)
+      aggregateApiResp <- aggregateService.orNotFound(apiRequest)
+      _ <- IO.println(aggregateApiResp)
+    yield ()
 
