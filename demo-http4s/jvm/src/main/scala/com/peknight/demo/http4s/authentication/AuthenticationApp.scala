ce3be32fcee16c601a82fe3152bbdcbc80aa7a93
peknight <JKpeknight@gmail.com>
2022-05-10 13:30:31 +0800

    http4s demo update authentication

--- demo-http4s/jvm/src/main/scala/com/peknight/demo/http4s/authentication/AuthenticationApp.scala
+++ demo-http4s/jvm/src/main/scala/com/peknight/demo/http4s/authentication/AuthenticationApp.scala
@@ -4,11 +4,12 @@ import cats.*
 import cats.data.*
 import cats.effect.*
 import cats.implicits.*
+import com.peknight.demo.http4s.runEmberServer
 import org.http4s.*
 import org.http4s.dsl.io.*
 import org.http4s.server.*
 
-object AuthenticationApp:
+object AuthenticationApp extends IOApp.Simple:
 
   given CanEqual[Path, Path] = CanEqual.derived
   given CanEqual[Method, Method] = CanEqual.derived
@@ -16,7 +17,8 @@ object AuthenticationApp:
   case class User(id: Long, name: String)
 
   // 从请求中获取登录用户信息，IO(???)表示获取用户信息的过程（比如读数据库之类的）
-  val authUser: Kleisli[[T] =>> OptionT[IO, T], Request[IO], User] = Kleisli(_ => OptionT.liftF(IO(???)))
+  val authUser: Kleisli[[T] =>> OptionT[IO, T], Request[IO], User] =
+    Kleisli(_ => OptionT.liftF(IO(User(123, "user_test"))))
 
   // 创建中间件
   val middleware: AuthMiddleware[IO, User] = AuthMiddleware(authUser)
@@ -37,8 +39,35 @@ object AuthenticationApp:
     case GET -> Root / "bonjour" => Ok(s"Bonjour")
   }
 
+  // AuthMiddleware会消费所有请求 请求不匹配路由或是未通过验证都会返回401。这里访问/bonjour就会401
   val serviceSpanish: HttpRoutes[IO] = middleware(spanishRoutes) <+> frenchRoutes
 
+  // 如果想不走验证正常访问/bonjour可以用以下方式
 
+  // 方法1 加固定前缀
+  val serviceRouter = Router(
+    "/spanish" -> middleware(spanishRoutes),
+    "/french" -> frenchRoutes
+  )
+  // 方法2 用withFallThrough
+  val middlewareWithFallThrough: AuthMiddleware[IO, User] = AuthMiddleware.withFallThrough(authUser)
+  val serviceSF: HttpRoutes[IO] = middlewareWithFallThrough(spanishRoutes) <+> frenchRoutes
+  // 方法3 把AuthedRoutes定义在最后
+  val serviceFS: HttpRoutes[IO] = frenchRoutes <+> middlewareWithFallThrough(spanishRoutes)
+  // 如果不想在验证失败时总是返回401，可以使用noSpider方法指定onAuthFailure handler
+  // AuthMiddleware.noSpider()
 
+  val authUserEither: Kleisli[IO, Request[IO], Either[String, User]] =
+    Kleisli(_ => IO(Either.right(User(123, "user_test"))))
+
+  val onFailure: AuthedRoutes[String, IO] = Kleisli(req => OptionT.liftF(Forbidden(req.context)))
+
+  val authMiddleware = AuthMiddleware(authUserEither, onFailure)
+
+  val serviceKleisli: HttpRoutes[IO] = authMiddleware(authedRoutes)
+
+  val run =
+    for
+      _ <- runEmberServer[IO](serviceFS.orNotFound)
+    yield ()
 
--- demo-http4s/jvm/src/main/scala/com/peknight/demo/http4s/middleware/MiddlewareApp.scala
+++ demo-http4s/jvm/src/main/scala/com/peknight/demo/http4s/middleware/MiddlewareApp.scala
@@ -4,6 +4,7 @@ import cats.data.Kleisli
 import cats.effect.*
 import cats.syntax.all.*
 import com.codahale.metrics.SharedMetricRegistries
+import com.peknight.demo.http4s.runEmberServer
 import org.http4s.*
 import org.http4s.dsl.io.*
 import org.http4s.implicits.*
@@ -102,5 +103,6 @@ object MiddlewareApp extends IOApp.Simple:
       requestIdResp <- responseIO
       _ <- IO.println(requestIdResp.headers)
       _ <- IO.println(requestIdResp.attributes.lookup(RequestId.requestIdAttrKey))
+      _ <- runEmberServer[IO]((aggregateService <+> meteredRoutes).orNotFound)
     yield ()
 
