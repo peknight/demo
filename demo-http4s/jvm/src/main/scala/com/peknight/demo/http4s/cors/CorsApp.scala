99962babad1c7e8c6b85d854b9548ab728fa4ebb
peknight <JKpeknight@gmail.com>
2022-05-11 09:54:03 +0800

    http4s update cors demo

--- demo-http4s/jvm/src/main/scala/com/peknight/demo/http4s/cors/CorsApp.scala
+++ demo-http4s/jvm/src/main/scala/com/peknight/demo/http4s/cors/CorsApp.scala
@@ -3,9 +3,15 @@ package com.peknight.demo.http4s.cors
 import cats.effect.*
 import org.http4s.*
 import org.http4s.dsl.io.*
+import org.http4s.headers.Origin
 import org.http4s.implicits.*
 import org.http4s.server.middleware.*
 
+import scala.concurrent.duration.*
+
+/**
+ * Cross origin resource sharing
+ */
 object CorsApp extends IOApp.Simple:
   val service = HttpRoutes.of[IO] {
     case _ => Ok()
@@ -15,11 +21,34 @@ object CorsApp extends IOApp.Simple:
 
   val corsService = CORS.policy.withAllowOriginAll(service)
 
+  val corsRequest = request.putHeaders("Origin" -> "https://somewhere.com")
+
+  val googleGet = Request[IO](Method.GET, uri"/", headers = Headers("Origin" -> "https://google.com"))
+
+  val yahooPut = Request[IO](Method.PUT, uri"/", headers = Headers("Origin" -> "https://yahoo.com"))
+
+  val duckPost = Request[IO](Method.POST, uri"/", headers = Headers("Origin" -> "https://docuduckgo.com"))
+
+  val corsMethodSvc = CORS.policy
+    .withAllowOriginAll
+    .withAllowMethodsIn(Set(Method.GET, Method.POST))
+    .withAllowCredentials(false)
+    .withMaxAge(1.day)
+    .apply(service)
+
   val run =
     for
-      rootResponse <- service.orNotFound(request)
-      _ <- IO.println(rootResponse)
-      corsResponse <- corsService.orNotFound(request)
+      rootResponse1 <- service.orNotFound(request)
+      _ <- IO.println(rootResponse1)
+      rootResponse2 <- corsService.orNotFound(request)
+      _ <- IO.println(rootResponse2)
+      corsResponse <- corsService.orNotFound(corsRequest)
       _ <- IO.println(corsResponse)
+      googleGetResp <- corsMethodSvc.orNotFound(googleGet)
+      _ <- IO.println(googleGetResp)
+      yahooPutResp <- corsMethodSvc.orNotFound(yahooPut)
+      _ <- IO.println(yahooPutResp)
+      duckPostResp <- corsMethodSvc.orNotFound(duckPost)
+      _ <- IO.println(duckPostResp)
     yield ()
 
