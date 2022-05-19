8a4ff8d72e22f98e31736f0baabde473f8828aa9
peknight <JKpeknight@gmail.com>
2022-05-19 10:26:04 +0800

    fix compile

--- demo-playground/jvm/src/main/scala/com/peknight/demo/playground/http4s/scalatags/ScalaTagsEntityApp.scala
+++ demo-playground/jvm/src/main/scala/com/peknight/demo/playground/http4s/scalatags/ScalaTagsEntityApp.scala
@@ -2,15 +2,15 @@ package com.peknight.demo.playground.http4s.scalatags
 
 import cats.effect.*
 import com.comcast.ip4s.*
+import org.http4s.{HttpRoutes, Method}
 import org.http4s.dsl.io.*
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.scalatags.*
 import org.http4s.server.middleware.Logger
 import org.http4s.server.staticcontent.*
-import org.http4s.HttpRoutes
-import org.http4s.Method
-import scalatags.Text.all.*
-import scalacss.ProdDefaults.*
+import scalatags.Text.all.{style as _, *}
+import scalatags.Text.tags2.style
+import scalacss.ProdDefaults.{cssStringRenderer, cssEnv}
 
 object ScalaTagsEntityApp extends IOApp.Simple:
 
@@ -18,7 +18,7 @@ object ScalaTagsEntityApp extends IOApp.Simple:
   given CanEqual[Method, Method] = CanEqual.derived
 
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root => Ok(html(head(style(CustomStyle.render)), body(h1("ScalaTagsEntityDemo"))))
+    case GET -> Root => Ok(html(head(style(CustomStyle.render[String])), body(h1("ScalaTagsEntityDemo"))))
   }
 
   val run =
