990b952bd7852ffb54537159e8b6eb310e2eb9c4
peknight <JKpeknight@gmail.com>
2022-06-22 09:24:54 +0800

    update debug info

--- demo-security/src/main/scala/com/peknight/demo/security/cipher/AesApp.scala
+++ demo-security/src/main/scala/com/peknight/demo/security/cipher/AesApp.scala
@@ -3,7 +3,6 @@ package com.peknight.demo.security.cipher
 import cats.effect.{IO, IOApp}
 import fs2.text.{base64, hex, utf8}
 import fs2.{Chunk, INothing, Pipe, Pull, Stream}
-import scodec.bits.ByteVector
 
 import java.math.BigInteger
 import java.nio.charset.StandardCharsets.UTF_8
@@ -23,23 +22,36 @@ object AesApp extends IOApp.Simple:
     new IvParameterSpec(iv)
   }
 
-  def chunkTimesN[F[_]](n: Int): Pipe[F, Byte, ByteVector] =
-    def go(acc: ByteVector, s: Stream[F, Byte]): Pull[F, INothing, Option[(ByteVector, Stream[F, Byte])]] =
+  def chunkTimesN[F[_], O](n: Int): Pipe[F, O, Chunk[O]] =
+    def go(acc: Chunk[O], s: Stream[F, O]): Pull[F, INothing, Option[(Chunk[O], Stream[F, O])]] =
       s.pull.uncons.flatMap {
         case None =>
-          if acc.isEmpty then Pull.pure(None)
-          else Pull.pure(Some((acc, Stream.empty)))
+          if acc.isEmpty then
+            println(s"chunkTimesN($n) None acc.isEmpty")
+            Pull.pure(None)
+          else
+            println(s"chunkTimesN($n) None acc = $acc")
+            Pull.pure(Some((acc, Stream.empty)))
         case Some((hd, tl)) =>
           val size = acc.size + hd.size
-          if size < n then go(acc ++ hd.toByteVector, tl)
-          else if size % n == 0 then Pull.pure(Some((acc ++ hd.toByteVector) -> tl))
+          if size < n then
+            println(s"chunkTimesN($n) Some(($hd, $tl)) acc = $acc $size < $n >> go")
+            go(acc ++ hd, tl)
+          else if size % n == 0 then
+            println(s"chunkTimesN($n) Some(($hd, $tl)) acc = $acc $size % $n == 0")
+            Pull.pure(Some((acc ++ hd) -> tl))
           else
-            val (pfx, sfx) = hd.splitAt((hd.size - size % n).toInt)
-            Pull.pure(Some((acc ++ pfx.toByteVector) -> tl.cons(sfx)))
+            val (pfx, sfx) = hd.splitAt(hd.size - size % n)
+            println(s"chunkTimesN($n) Some(($hd, $tl)) acc = $acc $size % $n != 0 pfx = $pfx sfx = $sfx")
+            Pull.pure(Some((acc ++ pfx) -> tl.cons(sfx)))
       }
-    in => in.repeatPull(pull => go(ByteVector.empty, pull.echo.stream).flatMap {
-      case Some((c, s)) => Pull.output1(c).as(Some(s))
-      case None => Pull.pure(None)
+    in => in.repeatPull(pull => go(Chunk.empty, pull.echo.stream).flatMap {
+      case Some((c, s)) =>
+        println(s"chunkTimesN($n) repeatPull Some(($c, $s))")
+        Pull.output1(c).as(Some(s))
+      case None =>
+        println(s"chunkTimesN($n) repeatPull None")
+        Pull.pure(None)
     })
   end chunkTimesN
 
@@ -49,10 +61,10 @@ object AesApp extends IOApp.Simple:
       private[this] val transformation = "AES/ECB/NoPadding"
 
       def encrypt[F[_]](key: => SecretKey): Pipe[F, Byte, Byte] = in =>
-        in.through(PKCS5Padding.padding).flatMap { bv =>
+        in.through(PKCS5Padding.padding).flatMap { chunk =>
           val cipher: Cipher = Cipher.getInstance(transformation)
           cipher.init(Cipher.ENCRYPT_MODE, key)
-          val output = cipher.doFinal(bv.toArray)
+          val output = cipher.doFinal(chunk.toArray)
           Stream.chunk(Chunk.array(output))
         }
 
@@ -61,7 +73,7 @@ object AesApp extends IOApp.Simple:
           val cipher: Cipher = Cipher.getInstance(transformation)
           cipher.init(Cipher.DECRYPT_MODE, key)
           val output = cipher.doFinal(chunk.toArray)
-          ByteVector(output)
+          Chunk.array(output)
         }.through(PKCS5Padding.unPadding)
 
       private[this] val javaTransformation = "AES/ECB/PKCS5Padding"
@@ -88,20 +100,20 @@ object AesApp extends IOApp.Simple:
         in.through(PKCS5Padding.padding).scanChunks(ivps) { (ivps, chunks) =>
           val cipher: Cipher = Cipher.getInstance(transformation)
           cipher.init(Cipher.ENCRYPT_MODE, key, ivps)
-          val output = cipher.doFinal(ByteVector.concat(chunks.iterator).toArray)
-          val outputVector = ByteVector(output)
-          val nextIv: Array[Byte] = outputVector.splitAt(outputVector.size - blockSize)._2.toArray
-          (new IvParameterSpec(nextIv), Chunk.array(output))
+          val output = cipher.doFinal(chunks.flatMap(identity).toArray)
+          val outputChunk = Chunk.array(output)
+          val nextIv: Array[Byte] = outputChunk.splitAt(outputChunk.size - blockSize)._2.toArray
+          (new IvParameterSpec(nextIv), outputChunk)
         }
 
       def decrypt[F[_]](key: => SecretKey, ivps: => IvParameterSpec): Pipe[F, Byte, Byte] = in =>
         in.through(chunkTimesN(blockSize)).scanChunks(ivps) { (ivps, chunks) =>
           val cipher: Cipher = Cipher.getInstance(transformation)
           cipher.init(Cipher.DECRYPT_MODE, key, ivps)
-          val output = cipher.doFinal(ByteVector.concat(chunks.iterator).toArray)
-          val outputVector = ByteVector(output)
-          val nextIv: Array[Byte] = outputVector.splitAt(outputVector.size - blockSize)._2.toArray
-          (new IvParameterSpec(nextIv), Chunk(outputVector))
+          val output = cipher.doFinal(chunks.flatMap(identity).toArray)
+          val outputChunk = Chunk.array(output)
+          val nextIv: Array[Byte] = outputChunk.splitAt(outputChunk.size - blockSize)._2.toArray
+          (new IvParameterSpec(nextIv), Chunk(outputChunk))
         }.through(PKCS5Padding.unPadding)
 
       def javaEncrypt(key: Array[Byte], ivps: IvParameterSpec, input: Array[Byte]): Array[Byte] =
@@ -120,40 +132,51 @@ object AesApp extends IOApp.Simple:
   end AES
 
   object PKCS5Padding:
-    def padding[F[_]]: Pipe[F, Byte, ByteVector] =
-      def go(acc: ByteVector, s: Stream[F, Byte]): Pull[F, INothing, Option[(ByteVector, Boolean, Stream[F, Byte])]] =
+    def padding[F[_]]: Pipe[F, Byte, Chunk[Byte]] =
+      def go(acc: Chunk[Byte], s: Stream[F, Byte]): Pull[F, INothing, Option[(Chunk[Byte], Boolean, Stream[F, Byte])]] =
         s.pull.uncons.flatMap {
           case None =>
             val paddingSize = AES.blockSize - acc.size
-            Pull.pure(Some((acc ++ ByteVector.fill(paddingSize)(paddingSize.toByte), true, Stream.empty)))
+            println(s"padding None acc = $acc paddingSize = $paddingSize")
+            Pull.pure(Some((acc ++ Chunk.seq(List.fill(paddingSize)(paddingSize.toByte)), true, Stream.empty)))
           case Some((hd, tl)) =>
             val size = acc.size + hd.size
-            if size < AES.blockSize then go(acc ++ hd.toByteVector, tl)
-            else if size % AES.blockSize == 0 then Pull.pure(Some((acc ++ hd.toByteVector, false, tl)))
+            if size < AES.blockSize then
+              println(s"padding Some(($hd, $tl)) acc = $acc $size < ${AES.blockSize} >> go")
+              go(acc ++ hd, tl)
+            else if size % AES.blockSize == 0 then
+              println(s"padding Some(($hd, $tl)) acc = $acc $size % ${AES.blockSize} == 0")
+              Pull.pure(Some((acc ++ hd, false, tl)))
             else
-              val (pfx, sfx) = hd.splitAt((hd.size - size % AES.blockSize).toInt)
-              Pull.pure(Some((acc ++ pfx.toByteVector, false, tl.cons(sfx))))
+              val (pfx, sfx) = hd.splitAt(hd.size - size % AES.blockSize)
+              println(s"padding Some(($hd, $tl)) acc = $acc $size % ${AES.blockSize} != 0 pfx = $pfx sfx = $sfx")
+              Pull.pure(Some((acc ++ pfx, false, tl.cons(sfx))))
         }
-      in => in.repeatPull(pull => go(ByteVector.empty, pull.echo.stream).flatMap {
-        case Some((bv, false, stream)) => Pull.output1(bv).as(Some(stream))
-        case Some((bv, _, _)) => Pull.output1(bv).as(None)
-        case None => Pull.pure(None)
+      in => in.repeatPull(pull => go(Chunk.empty, pull.echo.stream).flatMap {
+        case Some((chunk, false, stream)) =>
+          println(s"padding repeatPull Some(($chunk, false, _))")
+          Pull.output1(chunk).as(Some(stream))
+        case Some((chunk, _, _)) =>
+          println(s"padding repeatPull Some(($chunk, true, _))")
+          Pull.output1(chunk).as(None)
+        case None =>
+          println(s"padding repeatPull None")
+          Pull.pure(None)
       })
     end padding
 
-    def unPadding[F[_]]: Pipe[F, ByteVector, Byte] =
-      def go(acc: ByteVector, s: Stream[F, ByteVector]): Pull[F, Byte, Option[Stream[F, ByteVector]]] =
+    def unPadding[F[_]]: Pipe[F, Chunk[Byte], Byte] =
+      def go(acc: Chunk[Byte], s: Stream[F, Chunk[Byte]]): Pull[F, Byte, Option[Stream[F, Chunk[Byte]]]] =
         s.pull.uncons.flatMap {
           case None =>
-            val padSize: Byte = acc.lastOption.getOrElse(0)
+            val padSize: Byte = acc.last.getOrElse(0)
             assert((acc.size - padSize).until(acc.size).forall(acc(_) == padSize))
-            Pull.output(Chunk.byteVector(acc.dropRight(padSize))).flatMap(_ => Pull.pure(None))
+            Pull.output(acc.dropRight(padSize)).flatMap(_ => Pull.pure(None))
           case Some((hd, tl)) =>
             val (pfx, sfx) = hd.splitAt(hd.size - 1)
-            Pull.output(Chunk.byteVector(acc ++ ByteVector.concat(pfx.iterator)))
-              .flatMap(_ => go(ByteVector.concat(sfx.iterator), tl))
+            Pull.output(acc ++ pfx.flatMap(identity)).flatMap(_ => go(sfx.flatMap(identity), tl))
         }
-      in => in.repeatPull(pull => go(ByteVector.empty, pull.echo.stream))
+      in => in.repeatPull(pull => go(Chunk.empty, pull.echo.stream))
     end unPadding
   end PKCS5Padding
 
@@ -162,11 +185,11 @@ object AesApp extends IOApp.Simple:
       key <- keySpec("1234567890abcdef1234567890abcdef".getBytes(UTF_8))
       ivps <- ivParameterSpec
 
-      message = "Hello, world!" * 50
-      encryptedBytes = Stream(message).through(utf8.encode).through(AES.ECB.encrypt(key))
-      decryptedBytes = encryptedBytes.through(AES.ECB.decrypt(key))
-      encryptedHex = encryptedBytes.through(hex.encode).toList.mkString("")
-      decryptedMessage = decryptedBytes.through(utf8.decode).toList.mkString("")
+      message = "Hello, world!" * 2
+      // encryptedBytes = Stream(message).through(utf8.encode).through(AES.ECB.encrypt(key))
+      // decryptedBytes = encryptedBytes.through(AES.ECB.decrypt(key))
+      // encryptedHex = encryptedBytes.through(hex.encode).toList.mkString("")
+      // decryptedMessage = decryptedBytes.through(utf8.decode).toList.mkString("")
 
       cbcEncryptedBytes = Stream(message).through(utf8.encode).through(AES.CBC.encrypt(key, ivps))
       cbcDecryptedBytes = cbcEncryptedBytes.through(AES.CBC.decrypt(key, ivps))
@@ -174,10 +197,10 @@ object AesApp extends IOApp.Simple:
       cbcDecryptedMessage = cbcDecryptedBytes.through(utf8.decode).toList.mkString("")
 
       data = message.getBytes(UTF_8)
-      javaEncryptedBytes = AES.ECB.javaEncrypt(key.getEncoded, data)
-      javaDecryptedBytes = AES.ECB.javaDecrypt(key.getEncoded, javaEncryptedBytes)
-      javaEncryptedHex = new BigInteger(1, javaEncryptedBytes).toString(16)
-      javaDecryptedMessage = new String(javaDecryptedBytes, UTF_8)
+      // javaEncryptedBytes = AES.ECB.javaEncrypt(key.getEncoded, data)
+      // javaDecryptedBytes = AES.ECB.javaDecrypt(key.getEncoded, javaEncryptedBytes)
+      // javaEncryptedHex = new BigInteger(1, javaEncryptedBytes).toString(16)
+      // javaDecryptedMessage = new String(javaDecryptedBytes, UTF_8)
 
       javaCbcEncryptedBytes = AES.CBC.javaEncrypt(key.getEncoded, ivps, data)
       javaCbcDecryptedBytes = AES.CBC.javaDecrypt(key.getEncoded, ivps, javaCbcEncryptedBytes)
@@ -185,17 +208,17 @@ object AesApp extends IOApp.Simple:
       javaCbcDecryptedMessage = new String(javaCbcDecryptedBytes, UTF_8)
 
       _ <- IO.println(s"Data length             : ${data.length}")
-      _ <- IO.println(s"EncryptedHex            : $encryptedHex")
-      _ <- IO.println(s"JavaEncryptedHex        : $javaEncryptedHex")
+      // _ <- IO.println(s"EncryptedHex            : $encryptedHex")
+      // _ <- IO.println(s"JavaEncryptedHex        : $javaEncryptedHex")
       _ <- IO.println(s"CbcEncryptedHex         : $cbcEncryptedHex")
       _ <- IO.println(s"JavaCbcEncryptedHex     : $javaCbcEncryptedHex")
-      _ <- IO.println(s"Encrypted Correct       : ${Set(encryptedHex, javaEncryptedHex).size == 1}")
+      // _ <- IO.println(s"Encrypted Correct       : ${Set(encryptedHex, javaEncryptedHex).size == 1}")
       _ <- IO.println(s"CbcEncrypted Correct    : ${Set(cbcEncryptedHex, javaCbcEncryptedHex).size == 1}")
       _ <- IO.println(s"Message                 : $message")
-      _ <- IO.println(s"DecryptedMessage        : $decryptedMessage")
-      _ <- IO.println(s"JavaDecryptedMessage    : $javaDecryptedMessage")
+      // _ <- IO.println(s"DecryptedMessage        : $decryptedMessage")
+      // _ <- IO.println(s"JavaDecryptedMessage    : $javaDecryptedMessage")
       _ <- IO.println(s"CbcDecryptedMessage     : $cbcDecryptedMessage")
       _ <- IO.println(s"JavaCbcDecryptedMessage : $javaCbcDecryptedMessage")
-      _ <- IO.println(s"Decrypted Correct       : ${Set(message, decryptedMessage, javaDecryptedMessage).size == 1}")
+      // _ <- IO.println(s"Decrypted Correct       : ${Set(message, decryptedMessage, javaDecryptedMessage).size == 1}")
       _ <- IO.println(s"CbcDecrypted Correct    : ${Set(message, cbcDecryptedMessage, javaCbcDecryptedMessage).size == 1}")
     yield ()
