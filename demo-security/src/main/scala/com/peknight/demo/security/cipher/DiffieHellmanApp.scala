6fd1e467d3af36815815397faf0de34856d812b2
peknight <JKpeknight@gmail.com>
2022-07-01 12:05:40 +0800

    update diffie hellman demo

--- demo-security/src/main/scala/com/peknight/demo/security/cipher/DiffieHellmanApp.scala
+++ demo-security/src/main/scala/com/peknight/demo/security/cipher/DiffieHellmanApp.scala
@@ -1,8 +1,12 @@
 package com.peknight.demo.security.cipher
 
 import cats.effect.{IO, IOApp}
+import fs2.text.hex
+import fs2.{Chunk, Stream}
 
-import java.security.{PrivateKey, PublicKey}
+import java.security.*
+import java.security.spec.X509EncodedKeySpec
+import javax.crypto.KeyAgreement
 
 object DiffieHellmanApp extends IOApp.Simple:
 
@@ -30,9 +34,60 @@ object DiffieHellmanApp extends IOApp.Simple:
   // 甲计算：
   val s2: BigInt = B pow a mod p
 
+  val diffieHellmanAlgo = "DH"
+
+  // 生成本地KeyPair
+  val generateKeyPair: IO[KeyPair] = IO {
+    val kpGen: KeyPairGenerator = KeyPairGenerator.getInstance(diffieHellmanAlgo)
+    kpGen.initialize(512)
+    kpGen.generateKeyPair()
+  }
+
+  def generateSecretKey(privateKey: PrivateKey, receivedPubKeyBytes: Array[Byte]): IO[Array[Byte]] = IO {
+    // 从byte[]恢复PublicKey
+    val keySpec: X509EncodedKeySpec = X509EncodedKeySpec(receivedPubKeyBytes)
+    val kf: KeyFactory = KeyFactory.getInstance(diffieHellmanAlgo)
+    val receivedPublicKey: PublicKey = kf.generatePublic(keySpec)
+    // 生成本地密钥
+    val keyAgreement: KeyAgreement = KeyAgreement.getInstance(diffieHellmanAlgo)
+    // 自己的privateKey
+    keyAgreement.init(privateKey)
+    // 对方的PublicKey
+    keyAgreement.doPhase(receivedPublicKey, true)
+    // 生成SecretKey密钥
+    keyAgreement.generateSecret()
+  }
+
+
   // 最终双方协商出的密钥s1/s2是121
 
-  case class Person(name: String, publicKey: PublicKey, privateKey: PrivateKey, secretKey: Array[Byte])
+  case class Person(name: String, publicKey: PublicKey, privateKey: PrivateKey, secretKey: Array[Byte]):
+    private[this] def toHex(bytes: Array[Byte]): String =
+      Stream.chunk(Chunk.array(bytes)).through(hex.encode).toList.mkString("")
+    def show: String =
+      s"""
+         |Name: $name
+         |Private key: ${toHex(privateKey.getEncoded)}
+         |Public key: ${toHex(publicKey.getEncoded)}
+         |Secret key: ${toHex(secretKey)}
+      """.trim.stripMargin
+
+    val printKeys: IO[Unit] = IO.println(show)
+
+  val bobAndAlice: IO[(Person, Person)] =
+    for
+      // 各自生成KeyPair
+      bobKeyPair <- generateKeyPair
+      bobPublic = bobKeyPair.getPublic
+      bobPrivate = bobKeyPair.getPrivate
+
+      aliceKeyPair <- generateKeyPair
+      alicePublic = aliceKeyPair.getPublic
+      alicePrivate = aliceKeyPair.getPrivate
+
+      bobSecretKey <- generateSecretKey(bobPrivate, alicePublic.getEncoded)
+      aliceSecretKey <- generateSecretKey(alicePrivate, bobPublic.getEncoded)
+    yield (Person("Bob", bobPublic, bobPrivate, bobSecretKey), Person("Alice", alicePublic, alicePrivate, aliceSecretKey))
 
   val run: IO[Unit] =
     for
@@ -40,4 +95,10 @@ object DiffieHellmanApp extends IOApp.Simple:
       _ <- IO.println(B)
       _ <- IO.println(s1)
       _ <- IO.println(s2)
+      _ <- IO.println("--------")
+      bobAlice <- bobAndAlice
+      bob = bobAlice._1
+      alice = bobAlice._2
+      _ <- bob.printKeys
+      _ <- alice.printKeys
     yield ()
