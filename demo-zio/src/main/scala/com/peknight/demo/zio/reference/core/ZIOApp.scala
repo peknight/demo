7b00820a87d90a761b05747780059a7062d265d1
peknight <JKpeknight@gmail.com>
2023-09-17 23:00:46 +0800

    update zio app

--- demo-zio/src/main/scala/com/peknight/demo/zio/reference/core/ZIOApp.scala
+++ demo-zio/src/main/scala/com/peknight/demo/zio/reference/core/ZIOApp.scala
@@ -5,13 +5,39 @@ import zio.*
 object ZIOApp extends ZIOAppDefault:
 
   val s1 = ZIO.succeed(42)
-  val s2: Task[Int] = ZIO.succeed(42)
+
+  val f1 = ZIO.fail("Uh oh!")
+
+  val f2 = ZIO.fail(new Exception("Uh oh!"))
+
+  val zoption: IO[Option[Nothing], Int] = ZIO.fromOption(Some(2))
+
+  val zoption2: IO[String, Int] = zoption.mapError(_ => "It wasn't there!")
+
+  case class User(teamId: String)
+  case class Team()
+  val maybeId: IO[Option[Nothing], String] = ZIO.fromOption(Some("abc123"))
+  def getUser(userId: String): IO[Throwable, Option[User]] = ???
+  def getTeam(teamId: String): IO[Throwable, Team] = ???
+  def result: IO[Throwable, Option[(User, Team)]] = (
+    for
+      id <- maybeId
+      user <- getUser(id).some
+      team <- getTeam(user.teamId).asSomeError
+    yield (user, team)
+  ).unsome
 
   def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     for
       s1Res <- s1
       _ <- Console.printLine(s1Res)
-      s2Res <- s2
-      _ <- Console.printLine(s2Res)
+      f1Res <- f1.either
+      _ <- Console.printLine(f1Res)
+      f2Res <- f2.either
+      _ <- Console.printLine(f2Res)
+      zoptionRes <- zoption
+      _ <- Console.printLine(zoptionRes)
+      zoption2Res <- zoption
+      _ <- Console.printLine(zoption2Res)
     yield ()
 end ZIOApp
